4806dda3e275dcc7050bfde57a2fdcfc754b3557
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-01-13 16:52:48 -0500

    basic idea of threads working, now moving to supplier

--- src/main/java/org/example/Chef.java
+++ src/main/java/org/example/Chef.java
@@ -1,13 +1,27 @@
 package org.example;
 
 public class Chef implements Runnable {
-    enum Ingredient {
-        Rice, Nori, Filling
+//    enum Ingredient {
+//        Rice, Nori, Filling
+//    }
+
+    private String ingredient;
+
+    public Chef(String ingredient){
+        this.ingredient = ingredient;
     }
-
-
 
     public void run() {
         System.out.println("This code is running in a thread");
+        System.out.println(this.ingredient);
+    }
+
+    public static void main(String[] args) {
+        Thread chefThread1 = new Thread(new Chef("Rice"), "Chef with infinite rice");
+        Thread chefThread2 = new Thread(new Chef("Nori"), "Chef with infinite nori");
+        Thread chefThread3 = new Thread(new Chef("Filling"), "Chef with infinite filling");
+        chefThread1.start();
+        chefThread2.start();
+        chefThread3.start();
     }
 }
---
+++ src/main/java/org/example/Supplier.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Supplier implements Runnable{
+
+    public static void main(String[] args) {
+
+    }
+    public void run() {
+
+    }
+}
