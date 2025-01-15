899e8143cae5293fc58756278a1aff5a20403daf
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-01-15 01:37:05 -0500

    added comments for clarity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,26 @@
 package org.example;
 
 public class Main {
+    /**
+     * Main
+     * @param args args
+     */
     public static void main(String[] args) {
-        Counter counter = new Counter();
+        Counter counter = new Counter(); // create a counter to be shared between chef and agent
 
+        // create the 3 chef threads, each carrying infinite of one ingredient and link them to the counter
         Thread chefThread1 = new Thread(new Chef("Rice", counter), "Chef with infinite rice");
         Thread chefThread2 = new Thread(new Chef("Nori", counter), "Chef with infinite nori");
         Thread chefThread3 = new Thread(new Chef("Filling", counter), "Chef with infinite filling");
 
+        // start the chef threads and have them run concurrently
         chefThread1.start();
         chefThread2.start();
         chefThread3.start();
 
-
-        Thread supplierThread = new Thread(new Supplier(counter), "Supplier");
-        supplierThread.start();
+        // create the agent thread and link it to the same counter
+        Thread agentThread = new Thread(new Agent(counter), "Agent");
+        // start the agent thread
+        agentThread.start();
     }
 }
